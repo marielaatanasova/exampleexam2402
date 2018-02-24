412bff6556ba3aa0897a6daefe3686992c88af4d
Mariela Atanasova <mariela_2001at@abv.bg>
2018-02-24 13:41:06 +0200

    exam2402

---
+++ 17uprajnenie/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17uprajnenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int i = 0;
+            int step = numbers[i];
+            int sum = numbers[0];
+            do
+            {
+                if (i + step < numbers.Length && i + step >= 0)
+                {
+                    i = step + i;
+                    step = numbers[i];
+                    sum += numbers[i];
+                    Console.WriteLine(numbers[i]);
+
+                }
+                else
+                { 
+                if ((i + step >= numbers.Length || i - step <0 ) || (i + step <= 0 || i - step > numbers.Length))
+                {
+                    break;
+                }
+                
+                    step = -1*step;
+                }
+            } while (true);
+            Console.WriteLine(sum);
+        }
+
+    }
+}
+
+
---
+++ exam2402/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examexample2402
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            char[] resultCh = new char[10];
+
+            resultCh = input.ToCharArray();
+            Array.Reverse(resultCh);
+            Console.WriteLine($"input >>> {input} \nresult >>>>>> {string.Join("", resultCh)}");
+            string result = string.Join("", resultCh);
+            Console.WriteLine(result);
+
+          if (string.Equals(input,result)) //(string.Compare(input, result, true)==0)  //if (input == result)
+            {
+                Console.WriteLine("Suvpada");
+            }
+            result = input.Substring(1, 5);
+            Console.WriteLine($">>>>> {result}");
+
+            Array.Sort(resultCh);
+            Console.WriteLine($"input >>> {input} \nresult >>>>>> {string.Join("", resultCh)}");
+            Array.Clear(resultCh, 0, 2);
+            Console.WriteLine($"input >>> {input} \nresult >>>>>> {string.Join("", resultCh)}");
+
+
+
+        }
+    }
+}
